e554f85504b075db3504acc35fdf4069983259f9
denis <denisreypecoy@gmail.com>
2024-05-09 11:05:58 +0200

    Refactor send_telegram_message function to enhance reusability
    Modify the send_telegram_message function to accept API token and chat ID as parameters:
    1. Enhance the flexibility and reusability of the function by decoupling it from the config.json file.
    2. Streamline the process by passing the necessary credentials directly, allowing for easier integration in different contexts.
    3. Update the main execution block to load configuration before calling the function, ensuring credentials are managed externally.
    
    This change makes the function more versatile and secure, facilitating its use in varied operational environments.

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,21 +1,16 @@
 import requests
-import json
 
 
-def send_telegram_message(text):
+def send_telegram_message(text, api_token, chat_id):
     """
-    Sends a message to a specified Telegram chat using the API token from config.json.
-
+    Sends a message to a specified Telegram chat using the provided API token and chat ID.
     Args:
         text (str): The message content to be sent.
+        api_token (str): Telegram bot API token.
+        chat_id (str): Telegram chat ID.
+    Returns:
+        dict: The response from the Telegram API as a dictionary.
     """
-    # Load configuration
-    with open("config.json", "r") as f:
-        config = json.load(f)
-
-    api_token = config["telegram"]["api_token"]
-    chat_id = config["telegram"]["chat_id"]
-
     url = f"https://api.telegram.org/bot{api_token}/sendMessage"
     payload = {
         "chat_id": chat_id,
@@ -25,3 +20,23 @@ def send_telegram_message(text):
     }
     response = requests.post(url, data=payload)
     return response.json()
+
+
+if __name__ == "__main__":
+    from config_loader import find_config_path, load_configuration
+
+    config_path = find_config_path()
+    if config_path:
+        config = load_configuration(config_path)
+        if config and "telegram" in config:
+            api_token = config["telegram"]["api_token"]
+            chat_id = config["telegram"]["chat_id"]
+            send_telegram_message(
+                "Congratulations, the message to Telegram has been sent successfully",
+                api_token,
+                chat_id,
+            )
+        else:
+            print("Failed to load configuration.")
+    else:
+        print("Configuration file not found.")
